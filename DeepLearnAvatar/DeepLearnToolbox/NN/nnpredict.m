5a777af2865990d2661b2eab20ef154f37fca540
BobChew <azxbnb9s@gmail.com>
2014-08-06 17:27:44 +0800

    CNN Project

--- DeepLearnAvatar/DeepLearnToolbox/NN/nnpredict.m
+++
@@ -1,9 +0,0 @@
-function [labels,result] = nnpredict(nn, x)
-    nn.testing = 1;
-    nn = nnff(nn, x, zeros(size(x,1), nn.size(end)));
-    nn.testing = 0;
-    
-    [dummy, i] = max(nn.a{end},[],2);
-    labels = i;
-    result = nn.a{end};
-end
